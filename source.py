a6eae3a6b3f72081768ec79940f49804569afa0a
K.Udul Dewmina <165687248+KUDewmina@users.noreply.github.com>
2025-03-15 18:18:40 +0530

    Update source.py
    highlighted the way to generate substring for a given string for future projects

--- source.py
+++ source.py
@@ -3,10 +3,12 @@ class Solution:
         palindromes = set()
         max_lenght = 0
         longest_element = ''
+        #---------------------------- steps to generate all substrings for a given string
         n = len(s)
         for i in range(n):
             for j in range(i+1, n+1):
                 sub_string = s[i:j]
+        #---------------------------- ends here
                 if sub_string == sub_string[::-1] :
                     palindromes.add(sub_string)
         for element in palindromes:
